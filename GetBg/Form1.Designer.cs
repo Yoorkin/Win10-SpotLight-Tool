6ba675a21a724a06cc8b403de769dc363386af5d
yoorkin <937697555@qq.com>
2020-02-07 21:33:14 +0800

    first commit

---
+++ GetBg/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace GetBg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.setBtn = new System.Windows.Forms.Button();
+            this.cleanBtn = new System.Windows.Forms.Button();
+            this.openBtn = new System.Windows.Forms.Button();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.preBtn = new System.Windows.Forms.Button();
+            this.nextBtn = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // setBtn
+            // 
+            this.setBtn.Location = new System.Drawing.Point(67, 41);
+            this.setBtn.Name = "setBtn";
+            this.setBtn.Size = new System.Drawing.Size(162, 52);
+            this.setBtn.TabIndex = 0;
+            this.setBtn.Text = "设为桌面";
+            this.setBtn.UseVisualStyleBackColor = true;
+            this.setBtn.Click += new System.EventHandler(this.setBtn_Click);
+            // 
+            // cleanBtn
+            // 
+            this.cleanBtn.Location = new System.Drawing.Point(67, 113);
+            this.cleanBtn.Name = "cleanBtn";
+            this.cleanBtn.Size = new System.Drawing.Size(162, 52);
+            this.cleanBtn.TabIndex = 1;
+            this.cleanBtn.Text = "清空壁纸文件";
+            this.cleanBtn.UseVisualStyleBackColor = true;
+            this.cleanBtn.Click += new System.EventHandler(this.cleanBtn_Click);
+            // 
+            // openBtn
+            // 
+            this.openBtn.Location = new System.Drawing.Point(67, 185);
+            this.openBtn.Name = "openBtn";
+            this.openBtn.Size = new System.Drawing.Size(162, 52);
+            this.openBtn.TabIndex = 2;
+            this.openBtn.Text = "打开壁纸文件夹";
+            this.openBtn.UseVisualStyleBackColor = true;
+            this.openBtn.Click += new System.EventHandler(this.openBtn_Click);
+            // 
+            // picBox
+            // 
+            this.picBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picBox.Location = new System.Drawing.Point(309, 31);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(389, 221);
+            this.picBox.TabIndex = 3;
+            this.picBox.TabStop = false;
+            this.picBox.Click += new System.EventHandler(this.picBox_Click);
+            // 
+            // preBtn
+            // 
+            this.preBtn.Location = new System.Drawing.Point(262, 113);
+            this.preBtn.Name = "preBtn";
+            this.preBtn.Size = new System.Drawing.Size(41, 39);
+            this.preBtn.TabIndex = 4;
+            this.preBtn.Text = "<";
+            this.preBtn.UseVisualStyleBackColor = true;
+            this.preBtn.Click += new System.EventHandler(this.preBtn_Click);
+            // 
+            // nextBtn
+            // 
+            this.nextBtn.Location = new System.Drawing.Point(704, 113);
+            this.nextBtn.Name = "nextBtn";
+            this.nextBtn.Size = new System.Drawing.Size(41, 39);
+            this.nextBtn.TabIndex = 5;
+            this.nextBtn.Text = ">";
+            this.nextBtn.UseVisualStyleBackColor = true;
+            this.nextBtn.Click += new System.EventHandler(this.nextBtn_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkVisited = true;
+            this.linkLabel1.Location = new System.Drawing.Point(685, 272);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(79, 15);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "By Yorkin";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(12, 272);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(63, 15);
+            this.linkLabel2.TabIndex = 7;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "English";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 296);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.nextBtn);
+            this.Controls.Add(this.preBtn);
+            this.Controls.Add(this.picBox);
+            this.Controls.Add(this.openBtn);
+            this.Controls.Add(this.cleanBtn);
+            this.Controls.Add(this.setBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Windows10 SpotLight Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button setBtn;
+        private System.Windows.Forms.Button cleanBtn;
+        private System.Windows.Forms.Button openBtn;
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.Button preBtn;
+        private System.Windows.Forms.Button nextBtn;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+    }
+}
+
---
+++ GetBg/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace GetBg
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", EntryPoint = "SystemParametersInfoA")]
+        static extern Int32 SystemParametersInfo(Int32 uAction, Int32 uParam, string lpvParam, Int32 fuWinIni);
+        private const int SPI_SETDESKWALLPAPER = 20;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static string basePath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "\\锁屏壁纸";
+        static string phone = basePath + "\\phone";
+        static string desktop = basePath + "\\desktop";
+        static FileInfo[] imgView = new DirectoryInfo(desktop).GetFiles();
+        static int imgIndex = imgView.Length-1;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (Directory.Exists(basePath + "EN"))
+            {
+                Directory.CreateDirectory(basePath + "EN");
+                setBtn.Text = "Set as wallpaper";
+                openBtn.Text = "Reveal in explorer";
+                cleanBtn.Text = "Clear all pictures";
+                linkLabel2.Text = "中文";
+            }
+            linkLabel1.Links.Add(3, 9);
+            getImages();
+            picBox.Image = Image.FromFile(imgView[imgView.Length-1].FullName);
+        }
+        public static void getImages()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Packages";
+            List<string> dirs = new List<string>(Directory.EnumerateDirectories(path));
+            string content = "Microsoft.Windows.ContentDeliveryManager";
+            string target = "";
+            foreach (var file in new DirectoryInfo(path).GetDirectories())
+            {
+                if (file.Name.Length > content.Length && file.Name.Substring(0, content.Length) == content)
+                {
+                    target = file.FullName; ;
+                    break;
+                }
+            }
+            Directory.CreateDirectory(basePath);
+            Directory.CreateDirectory(desktop);
+            Directory.CreateDirectory(phone);
+            int count = 0;
+            foreach (var file in new DirectoryInfo(target + "\\LocalState\\Assets").GetFiles())
+            {
+                System.IO.File.Copy(file.FullName, basePath + "\\" + file.Name + ".jpg");
+            }
+            foreach (var imgFile in new DirectoryInfo(basePath).GetFiles())
+            {
+
+                Image pic = Image.FromFile(imgFile.FullName);
+                if (pic.Width < 1000)
+                {
+                    pic.Dispose();
+                    imgFile.Delete();
+                    continue;
+                }
+                if (pic.Width > pic.Height)
+                {
+                    pic.Dispose();
+                    pic.Dispose();
+                    string trg = desktop + "\\" + imgFile.Name;
+                    if (File.Exists(trg))
+                        imgFile.Delete();
+                    else
+                        File.Move(imgFile.FullName, trg);
+                }
+                else
+                {
+                    pic.Dispose();
+                    string trg = phone + "\\" + imgFile.Name;
+                    if (File.Exists(trg))
+                        imgFile.Delete();
+                    else
+                        File.Move(imgFile.FullName, trg);
+                }
+            }
+        }
+
+        private void setBtn_Click(object sender, EventArgs e)
+        {
+            string tmp = basePath + "\\使用中的壁纸.jpg";
+            File.Delete(tmp);
+            File.Copy(imgView[imgIndex%(imgView.Length-1)].FullName, tmp);
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, tmp , 1); ;
+        }
+
+        private void cleanBtn_Click(object sender, EventArgs e)
+        {
+            DialogResult result;
+            if (Directory.Exists(basePath + "EN"))
+                result = MessageBox.Show("Are you sure to clear all pictures?", "Attention", MessageBoxButtons.OKCancel);
+            else
+                result = MessageBox.Show("确定要清空Windows聚焦壁纸吗？\n（包括以前保存的壁纸)", "提示", MessageBoxButtons.OKCancel);
+            if(result == DialogResult.OK)
+            {
+                foreach (var file in new DirectoryInfo(phone).GetFiles()) file.Delete();
+                foreach (var file in new DirectoryInfo(desktop).GetFiles()) file.Delete();
+            }
+        }
+
+        private void openBtn_Click(object sender, EventArgs e)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "explorer.exe";
+            p.StartInfo.Arguments = basePath;
+            p.Start();
+        }
+
+        private void preBtn_Click(object sender, EventArgs e)
+        {
+            picBox.Image.Dispose();
+            imgIndex--;
+            if (imgIndex == 0) imgIndex = imgView.Length - 1;
+            picBox.Image = Image.FromFile(imgView[imgIndex % (imgView.Length - 1)].FullName);
+            picBox.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void nextBtn_Click(object sender, EventArgs e)
+        {
+            picBox.Image.Dispose();
+            imgIndex++;
+            picBox.Image = Image.FromFile(imgView[imgIndex % (imgView.Length - 1)].FullName);
+            picBox.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void picBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("explorer.exe", @"https://github.com/yoorkin");
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if(Directory.Exists(basePath + "EN"))
+            {
+                Directory.Delete(basePath + "EN");
+                setBtn.Text = "设为壁纸";
+                openBtn.Text = "打开壁纸文件夹";
+                cleanBtn.Text = "清空壁纸";
+                linkLabel2.Text = "English";
+            }
+            else
+            {
+                Directory.CreateDirectory(basePath + "EN");
+                setBtn.Text = "Set as wallpaper";
+                openBtn.Text = "Reveal in explorer";
+                cleanBtn.Text = "Clear all pictures";
+                linkLabel2.Text = "中文";
+            }
+        }
+    }
+}
